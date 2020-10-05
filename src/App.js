bb6a651cd78fd7d3c28db87f59c82fd830c4acaa
Reece Dunham <me@rdil.rocks>
2020-10-05 20:19:17 +0000

    Working dark theme

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from "react"
-import banner from './banner.png'; // banner
-//import icon from './icon.png'; // banner
+import banner from "./banner.png" // banner
 
 //Main title commented below
 //<Typography variant="h2">The IfOnly Guild</Typography>
-                /* {
+/* {
                   <Centered>
                   <div>
                   <img src={icon} width="200" height="200" alt="IfOnly Banner" />
@@ -13,7 +12,6 @@ import banner from './banner.png'; // banner
                   }
                   <Divider style={{ margin: 20 }} /> */
 
-
 import {
     AppBar,
     Toolbar,
@@ -21,80 +19,69 @@ import {
     Grid,
     Button,
     Divider,
+    FormControlLabel,
+    Switch,
+    Paper
 } from "@material-ui/core"
 import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles"
 import { Chat } from "@material-ui/icons"
-import green from "@material-ui/core/colors/green"
-import blue from "@material-ui/core/colors/blue"
 
-//DARK MODE
-import Darkmode from 'darkmode-js';
-
-const options = {
-  bottom: '10px', // default: '32px'
-  right: '10px', // default: '32px'
-  left: 'unset', // default: 'unset'
-  time: '0.5s', // default: '0.3s'
-  mixColor: '#fff', // default: '#fff'
-  backgroundColor: '#fff',  // default: '#fff'
-  buttonColorDark: '#100f2c',  // default: '#100f2c'
-  buttonColorLight: '#fff', // default: '#fff'
-  saveInCookies: true, // default: true,
-  label: '🌓', // default: ''
-  autoMatchOsTheme: true // default: true
-}
-const darkmode = new Darkmode(options);
-darkmode.showWidget();
-//DARK MODE
-
-const theme = createMuiTheme({
+const darkPalette = createMuiTheme({
     palette: {
-        primary: {
-            main: green[700],
-        },
-        secondary: blue,
+        type: "dark",
     },
 })
 
-const navbarItems = [
+const navbarItems = (darkTheme, setDarkTheme) => [
     <a href="https://discord.gg/DmAqWfQ" className="navlink">
         <Button color="inherit" startIcon={<Chat />}>
             Discord
         </Button>
     </a>,
+    <FormControlLabel
+        control={<Switch checked={darkTheme} onChange={() => setDarkTheme(darkTheme ? false : true)} name="darkTheme" />}
+        label="Dark Theme"
+    />
 ]
 
 export default () => {
-    let Centered = (props) => (
+    const [darkTheme, setDarkTheme] = React.useState(false)
+
+    const Centered = (props) => (
         <Grid container justify="center">
             {props.children}
         </Grid>
     )
 
-    return (
-        <ThemeProvider theme={theme}>
+    const component = (
+        <>
             <nav style={{ flexGrow: 1 }}>
                 <AppBar position="static">
                     <Toolbar>
-
                         <Typography style={{ flexGrow: 1 }} variant="h6">
                             The IfOnly Guild
                         </Typography>
-                        {navbarItems}
+                        {navbarItems(darkTheme, setDarkTheme)}
                     </Toolbar>
                 </AppBar>
             </nav>
-            <main className="main">
+            <main>
+                <Paper style={{ borderRadius: "0px", padding: "15px" }}>
                 <Grid container>
                     <Centered>
                         {
-                        <div>
-                            <img src={banner} width="719.875" height="191.875" alt="IfOnly Banner" />
-                        </div>
+                            <div>
+                                <img
+                                    src={banner}
+                                    width="719.875"
+                                    height="191.875"
+                                    alt="IfOnly Banner"
+                                />
+                            </div>
                         }
                     </Centered>
                     <Centered>
-                        <Typography variant="subtitle">
+                        <Typography variant="body2">
                             A Hypixel guild based on the community.
                         </Typography>
                     </Centered>
@@ -107,7 +94,8 @@ export default () => {
                     <Centered>
                         <Typography
                             variant="body1"
-                            style={{ textAlign: "center" }}>
+                            style={{ textAlign: "center" }}
+                        >
                             IfOnly is a Hypixel guild founded back in early
                             2018. It grew quite big over time and eventually
                             surpased 90 members. Originally, the guild was based
@@ -123,12 +111,15 @@ export default () => {
                 <Divider style={{ margin: 5 }} />
                 <Grid container>
                     <Centered>
-                        <Typography variant="h5" style={{ marginTop: 20 }}>Benefits</Typography>
+                        <Typography variant="h5" style={{ marginTop: 20 }}>
+                            Benefits
+                        </Typography>
                     </Centered>
                     <Centered>
                         <Typography
                             variant="body1"
-                            style={{ textAlign: "center" }}>
+                            style={{ textAlign: "center" }}
+                        >
                             Our guild has a lot of things to offer to our
                             members, including weekly game nights, an SMP
                             server, the [I] tag on Hypixel, and lots of friendly
@@ -139,13 +130,16 @@ export default () => {
                 <Divider style={{ margin: 5 }} />
                 <Grid container>
                     <Centered>
-                        <Typography variant="h5" style={{ marginTop: 20 }}>Join Us</Typography>
+                        <Typography variant="h5" style={{ marginTop: 20 }}>
+                            Join Us
+                        </Typography>
                     </Centered>
                     <Divider style={{ margin: 5 }} />
                     <Centered>
                         <Typography
                             variant="body1"
-                            style={{ textAlign: "center" }}>
+                            style={{ textAlign: "center" }}
+                        >
                             If you want to join, feel free! Simply join our
                             Discord, tell us a bit about yourself, and you will
                             get the benefits we have to offer!
@@ -153,7 +147,14 @@ export default () => {
                     </Centered>
                 </Grid>
                 <Divider style={{ margin: 5 }} />
+                </Paper>
             </main>
-        </ThemeProvider>
+        </>
     )
+
+    if (darkTheme) {
+        return <ThemeProvider theme={darkPalette}>{component}</ThemeProvider>
+    }
+
+    return component
 }
