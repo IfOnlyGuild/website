d65274686a7508ad58280898dbb1f100daf49629
Reece Dunham <me@rdil.rocks>
2020-10-05 20:25:41 +0000

    Run prettier

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ import {
     Divider,
     FormControlLabel,
     Switch,
-    Paper
+    Paper,
 } from "@material-ui/core"
 import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles"
 import { Chat } from "@material-ui/icons"
@@ -39,9 +39,15 @@ const navbarItems = (darkTheme, setDarkTheme) => [
         </Button>
     </a>,
     <FormControlLabel
-        control={<Switch checked={darkTheme} onChange={() => setDarkTheme(darkTheme ? false : true)} name="darkTheme" />}
+        control={
+            <Switch
+                checked={darkTheme}
+                onChange={() => setDarkTheme(darkTheme ? false : true)}
+                name="darkTheme"
+            />
+        }
         label="Dark Theme"
-    />
+    />,
 ]
 
 export default () => {
@@ -67,86 +73,87 @@ export default () => {
             </nav>
             <main>
                 <Paper style={{ borderRadius: "0px", padding: "15px" }}>
-                <Grid container>
-                    <Centered>
-                        {
-                            <div>
-                                <img
-                                    src={banner}
-                                    width="719.875"
-                                    height="191.875"
-                                    alt="IfOnly Banner"
-                                />
-                            </div>
-                        }
-                    </Centered>
-                    <Centered>
-                        <Typography variant="body2">
-                            A Hypixel guild based on the community.
-                        </Typography>
-                    </Centered>
-                </Grid>
-                <Divider style={{ margin: 5 }} />
-                <Grid container>
-                    <Centered>
-                        <Typography variant="h5">About</Typography>
-                    </Centered>
-                    <Centered>
-                        <Typography
-                            variant="body1"
-                            style={{ textAlign: "center" }}
-                        >
-                            IfOnly is a Hypixel guild founded back in early
-                            2018. It grew quite big over time and eventually
-                            surpased 90 members. Originally, the guild was based
-                            more on minigame skill as well as competitive
-                            statistics, but over time we realized that we have
-                            something special: our community. We have since
-                            stopped requiring high statistics/skill levels, and
-                            now focus primarily on our players. We regularly
-                            host events, giveaways, and more!
-                        </Typography>
-                    </Centered>
-                </Grid>
-                <Divider style={{ margin: 5 }} />
-                <Grid container>
-                    <Centered>
-                        <Typography variant="h5" style={{ marginTop: 20 }}>
-                            Benefits
-                        </Typography>
-                    </Centered>
-                    <Centered>
-                        <Typography
-                            variant="body1"
-                            style={{ textAlign: "center" }}
-                        >
-                            Our guild has a lot of things to offer to our
-                            members, including weekly game nights, an SMP
-                            server, the [I] tag on Hypixel, and lots of friendly
-                            faces to play and chat with!
-                        </Typography>
-                    </Centered>
-                </Grid>
-                <Divider style={{ margin: 5 }} />
-                <Grid container>
-                    <Centered>
-                        <Typography variant="h5" style={{ marginTop: 20 }}>
-                            Join Us
-                        </Typography>
-                    </Centered>
+                    <Grid container>
+                        <Centered>
+                            {
+                                <div>
+                                    <img
+                                        src={banner}
+                                        width="719.875"
+                                        height="191.875"
+                                        alt="IfOnly Banner"
+                                    />
+                                </div>
+                            }
+                        </Centered>
+                        <Centered>
+                            <Typography variant="body2">
+                                A Hypixel guild based on the community.
+                            </Typography>
+                        </Centered>
+                    </Grid>
                     <Divider style={{ margin: 5 }} />
-                    <Centered>
-                        <Typography
-                            variant="body1"
-                            style={{ textAlign: "center" }}
-                        >
-                            If you want to join, feel free! Simply join our
-                            Discord, tell us a bit about yourself, and you will
-                            get the benefits we have to offer!
-                        </Typography>
-                    </Centered>
-                </Grid>
-                <Divider style={{ margin: 5 }} />
+                    <Grid container>
+                        <Centered>
+                            <Typography variant="h5">About</Typography>
+                        </Centered>
+                        <Centered>
+                            <Typography
+                                variant="body1"
+                                style={{ textAlign: "center" }}
+                            >
+                                IfOnly is a Hypixel guild founded back in early
+                                2018. It grew quite big over time and eventually
+                                surpased 90 members. Originally, the guild was
+                                based more on minigame skill as well as
+                                competitive statistics, but over time we
+                                realized that we have something special: our
+                                community. We have since stopped requiring high
+                                statistics/skill levels, and now focus primarily
+                                on our players. We regularly host events,
+                                giveaways, and more!
+                            </Typography>
+                        </Centered>
+                    </Grid>
+                    <Divider style={{ margin: 5 }} />
+                    <Grid container>
+                        <Centered>
+                            <Typography variant="h5" style={{ marginTop: 20 }}>
+                                Benefits
+                            </Typography>
+                        </Centered>
+                        <Centered>
+                            <Typography
+                                variant="body1"
+                                style={{ textAlign: "center" }}
+                            >
+                                Our guild has a lot of things to offer to our
+                                members, including weekly game nights, an SMP
+                                server, the [I] tag on Hypixel, and lots of
+                                friendly faces to play and chat with!
+                            </Typography>
+                        </Centered>
+                    </Grid>
+                    <Divider style={{ margin: 5 }} />
+                    <Grid container>
+                        <Centered>
+                            <Typography variant="h5" style={{ marginTop: 20 }}>
+                                Join Us
+                            </Typography>
+                        </Centered>
+                        <Divider style={{ margin: 5 }} />
+                        <Centered>
+                            <Typography
+                                variant="body1"
+                                style={{ textAlign: "center" }}
+                            >
+                                If you want to join, feel free! Simply join our
+                                Discord, tell us a bit about yourself, and you
+                                will get the benefits we have to offer!
+                            </Typography>
+                        </Centered>
+                    </Grid>
+                    <Divider style={{ margin: 5 }} />
                 </Paper>
             </main>
         </>
